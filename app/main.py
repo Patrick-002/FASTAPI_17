ea59ef3c21d457426f4d717071b4886a57f51e72
Kirill <kirilltymanov2000@gmail.com>
2024-12-22 16:09:38 +0300

    module_17_2 done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routers import task, user
+
 app = FastAPI()
 app.include_router(task.router)
 app.include_router(user.router)
+
 
 @app.get("/")
 async def welcome():
